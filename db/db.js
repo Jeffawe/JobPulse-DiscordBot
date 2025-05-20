517cfc49493702ac8730bacef926ef1c4e0f5586
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-05-20 00:28:51 -0400

    Switched DB States

--- db/db.js
+++ db/db.js
@@ -1,8 +1,21 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import path from 'path';
+import dotenv from 'dotenv';
+import os from 'os';
 
-const dbPath = '../db/database.sqlite';
+dotenv.config();
+
+db_state = process.env.DB_STATE;
+let dbPath;
+
+if (db_state === 'local') {
+  dbPath = '../db/database.sqlite';
+} else {
+  const homeDir = os.homedir(); // "/home/somua"
+  dbPath = path.resolve(homeDir, 'sqldb/database.sqlite');
+}
+
 let dbInstance;
 
 export const connectDB = async () => {
